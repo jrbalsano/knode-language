4885a92c3dbea1083fefaa11d579f434caadd35f
Jonathan Balsano <jrbalsano@gmail.com>
2013-04-29 00:56:20 -0400

    Cleaning up and commenting

--- absyn.c
+++ absyn.c
@@ -1,12 +1,27 @@
 #include "absyn.h"
 #include <stdlib.h>
 
+/*******************
+ * Translation Units
+ *******************/
+
+/**
+ * Create Translation Unit from an existing FunctionDefinition
+ */
 TranslationUnit getTranslationUnit(FunctionDefinition fd) {
   TranslationUnit ret = (TranslationUnit)malloc(sizeof(struct translationUnit_));
   ret->f = fd;
   return ret;
 }
 
+/*********************
+ * Function Definition 
+ *********************/
+
+/**
+ * Create FunctionDefinition from its Declaration and the statements that comprise
+ * the function.
+ */
 FunctionDefinition getFunctionDefinition(Declarator d, CompoundStatement cs) {
   FunctionDefinition ret = (FunctionDefinition)malloc(sizeof(struct functionDefinition_));
   ret->d = d;
@@ -14,12 +29,23 @@ FunctionDefinition getFunctionDefinition(Declarator d, CompoundStatement cs) {
   return ret;
 }
 
+/*************
+ * Declarators
+ *************/
+
+/**
+ * Create a declarator from just an identifier. This is used in cases where
+ * the function has no parameters, just an identifier
+ */
 Declarator declaratorId(Identifier id) {
   Declarator d = (Declarator)malloc(sizeof(struct declarator_));
   d->name = id;
   return d;
 }
 
+/**
+ * Create a declarator from an identifier and a parameter list
+ */
 Declarator getDeclarator(Identifier id, GrammarList pList) {
   Declarator d = (Declarator)malloc(sizeof(struct declarator_));
   d->name = id;
@@ -27,13 +53,27 @@ Declarator getDeclarator(Identifier id, GrammarList pList) {
   return d;
 }
 
+/*********************
+ * Compound Statements
+ *********************/
 
+/**
+ * Create a new compound statement by initializing a new statement list.
+ */
 CompoundStatement newCompoundStatement(GrammarList sList) {
   CompoundStatement ret = (CompoundStatement)malloc(sizeof(struct compoundStatement_));
   ret->sList = sList;
   return ret;
 }
 
+/*****************
+ * Grammar Lists
+ *****************/
+
+/**
+ * Create a new statement list from an existing statement. To be used in situations where
+ * a statement list does not already exist.
+ */
 GrammarList newStatementList(Statement s) {
   GrammarList sList = (GrammarList)malloc(sizeof(struct grammarList_));
   sList->head = 0;
@@ -41,6 +81,34 @@ GrammarList newStatementList(Statement s) {
   return sList;
 }
 
+/**
+ * Creates a new argument expression list from an existing expression. To be used in
+ * situations where the argument expression list does not already exist.
+ */
+GrammarList newArgumentExpressionList(Expression e) {
+  GrammarList aeList = (GrammarList)malloc(sizeof(struct grammarList_));
+  aeList->head = 0;
+  addFront(aeList, e);
+  return aeList;
+}
+
+/**
+ * Add a node to the front of the Grammar List g, with data pointer data
+ */
+void addFront(GrammarList g, void *data) {
+  GrammarNode n = (GrammarNode)malloc(sizeof(struct grammarNode_));
+  n->data = data;
+  n->next = g->head;
+  g->head = n;
+}
+
+/************
+ * Statements
+ ************/
+
+/**
+ * Create a Statement from an existing expression
+ */
 Statement getExpressionStatement(Expression e) {
   Statement s = (Statement)malloc(sizeof(struct statement_));
   s->type = expression;
@@ -48,6 +116,14 @@ Statement getExpressionStatement(Expression e) {
   return s;
 }
 
+/*************
+ * Expressions
+ *************/
+
+/**
+ * Create a new expression from a function call defined by the functions id
+ * and the list of arguments
+ */
 Expression getFunctionExpression(Identifier id, GrammarList argExpList) {
   Expression ret = (Expression)malloc(sizeof(struct expression_));
   ret->type = function;
@@ -56,6 +132,9 @@ Expression getFunctionExpression(Identifier id, GrammarList argExpList) {
   return ret;
 }
 
+/**
+ * Creates a new primary expression from an existing identifier
+ */
 Expression getPrimaryIdentifierExpression(Identifier id){
   Expression ret = (Expression)malloc(sizeof(struct expression_));
   ret->type = primary;
@@ -63,6 +142,9 @@ Expression getPrimaryIdentifierExpression(Identifier id){
   return ret;
 }
 
+/**
+ * Creates a new primary expression from an existing string
+ */
 Expression getPrimaryStringExpression(char *s) {
   Expression ret = (Expression)malloc(sizeof(struct expression_));
   ret->type = string;
@@ -70,19 +152,9 @@ Expression getPrimaryStringExpression(char *s) {
   return ret;
 }
 
-GrammarList newArgumentExpressionList(Expression e) {
-  GrammarList aeList = (GrammarList)malloc(sizeof(struct grammarList_));
-  aeList->head = 0;
-  addFront(aeList, e);
-  return aeList;
-}
-
-Identifier getIdentifier(char *s) {
-  Identifier i = (Identifier)malloc(sizeof(struct identifier_));
-  i->symbol = s;
-  return i;
-}
-
+/**
+ * Creates a new Postfix Expression from an existing expression
+ */
 Expression getPostfixExpression(Expression e1){
   Expression ret = (Expression)malloc(sizeof(struct expression_));
   ret->deriv.postfix = none;
@@ -91,6 +163,10 @@ Expression getPostfixExpression(Expression e1){
   return ret;
 }
 
+/**
+ * Creates a new Postfix Expression from an existing expression followed
+ * by square brackets encompassing a second expression
+ */
 Expression getPostfixBracketExpression(Expression e1, Expression e2){
   Expression ret = (Expression)malloc(sizeof(struct expression_));
   ret->type = postfix;
@@ -100,6 +176,10 @@ Expression getPostfixBracketExpression(Expression e1, Expression e2){
   return ret;
 }
 
+/**
+ * Creates a new Postfix Expression from an existing argument list and an
+ * existing expression
+ */
 Expression getPostfixArgumentExpression(Expression e1, GrammarList argList){
   Expression ret = (Expression)malloc(sizeof(struct expression_));
   ret->type = postfix;
@@ -110,6 +190,10 @@ Expression getPostfixArgumentExpression(Expression e1, GrammarList argList){
 
 }
 
+/**
+ * Creates a new Postfix Expression from an existing expression and specifies
+ * it as an incremement expression
+ */
 Expression getPostfixIncr(Expression e){
   Expression ret = (Expression)malloc(sizeof(struct expression_));
   ret->deriv.postfix = increment;
@@ -117,7 +201,10 @@ Expression getPostfixIncr(Expression e){
   return ret;
 }
 
-
+/**
+ * Creates a new Postfix Expression from an existing expression and specifies
+ * it as an increment expression
+ */
 Expression getPostfixDecr(Expression e){
   Expression ret = (Expression)malloc(sizeof(struct expression_));
   ret->deriv.postfix = decrement;
@@ -125,6 +212,11 @@ Expression getPostfixDecr(Expression e){
   return ret;
 }
 
+/**
+ * Creates a new Postfix Expression from an existing expression and its
+ * identifier. This is to be used when accessing the properties of some
+ * expression using a expression.identifier construct.
+ */
 Expression getPostfixIdentifierExpression(Expression e, Identifier id){
   Expression ret = (Expression)malloc(sizeof(struct expression_));
   ret->type = postfix;
@@ -134,10 +226,17 @@ Expression getPostfixIdentifierExpression(Expression e, Identifier id){
   return ret;
 }
 
-void addFront(GrammarList g, void *data) {
-  GrammarNode n = (GrammarNode)malloc(sizeof(struct grammarNode_));
-  n->data = data;
-  n->next = g->head;
-  g->head = n;
+/*************
+ * Identifiers
+ *************/
+
+/**
+ * Create an identifier from a string
+ * TODO: Add symbol table functionality
+ */
+Identifier getIdentifier(char *s) {
+  Identifier i = (Identifier)malloc(sizeof(struct identifier_));
+  i->symbol = s;
+  return i;
 }
 
