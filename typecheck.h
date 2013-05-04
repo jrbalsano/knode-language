6f7f8f2d98b665588ae2468da1ee0b79bc47b306
Jonathan Balsano <jrbalsano@gmail.com>
2013-05-04 14:10:57 -0400

    Adding descriptions for functions

--- typecheck.h
+++ typecheck.h
@@ -7,35 +7,78 @@
 /************************************************
  * TranslationUnits
  ************************************************/
+/**
+ * This function likely doesn't need to do anything so lets
+ * just leave it here for now in case we need it but don't implement
+ * it.
+ */
 void translationUnitTypeCheck(TranslationUnit t);
 
 /************************************************
  * FunctionDefinitions
  ************************************************/
+/**
+ * This function should probably make sure that its function type and
+ * return type match up. Any other type inconsistencies are hard to do
+ * anything about.
+ */
 void functionDefinitionTypeCheck(FunctionDefinition f);
 
 /************************************************
  * Declarators
  ************************************************/
+
+/**
+ * This need only figure out what kind of declarator it is - what its
+ * identifier is, what kind of parameters it has, and what its return
+ * type should be.
+ */
 void declaratorTypeCheck(Declarator d);
 
 /************************************************
  * Compounds Statements
  ************************************************/
+/**
+ * This probably just needs to pass up a type for whatever kind
+ * of grammar list it has.
+ */
 void compoundStatementTypeCheck(CompoundStatement cs);
 
 /************************************************
  * Grammar Lists
  ************************************************/
+
+/**
+ * Doesn't need to do anything?
+ */ 
 void expressionListTypeCheck(GrammarList g);
+
+/**
+ * Doesn't need to do anything?
+ */
 void statementListTypeCheck(GrammarList g);
+
+/**
+ * Should pick up some sort of combined type, like a list
+ * of types, to denote the parameters.
+ */
 void parameterListTypeCheck(GrammarList g);
 
 /************************************************
  * Statements
  ************************************************/
-/* Iteration */
+
+/**
+ * Checks to make sure that each statement that is in
+ * its compound statement is a valid statement, and not
+ * a dictionary definition for example. Need not be recursive.
+ * Also checks the middle expression to make sure it types to
+ * boolean or indeterminable.
+ */
 void forStatementTypeCheck(Statement s);
+
+/**
+ * 
 void whileStatementTypeCheck(Statement s);
 /* Selection */
 void ifStatementTypeCheck(Statement s);
