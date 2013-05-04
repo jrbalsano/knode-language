b546cbad63e02e1e75a29c82032b80976be86c6a
Jonathan Balsano <jrbalsano@gmail.com>
2013-05-04 16:10:03 -0400

    Added empty method bodies

---
+++ codegen.c
@@ -0,0 +1,150 @@
+#include "codegen.h"
+
+void translationUnitGenerateCode(translationUnit t) {
+
+}
+
+void functionDefinitionGenerateCode(FunctionDefinition f) {
+
+}
+
+void declaratorGenerateCode(Declarator d) {
+
+}
+
+void compoundStatementGenerateCode(CompoundStatement cs) {
+
+}
+
+void expressionListGenerateCode(GrammarList g) {
+
+}
+
+void statementListGenerateCode(GrammarList g) {
+
+}
+
+void parameterListGenerateCode(GrammarList g) {
+
+}
+
+void forStatementGenerateCode(Statement s) {
+
+}
+
+void whileStatementGenerateCode(Statement s) {
+
+}
+
+void ifStatementGenerateCode(Statement s) {
+
+}
+
+void ifelseStatementGenerateCode(Statement s) {
+
+}
+
+void dictlistGenerateCode(Statement s) {
+
+}
+
+void dictDefinitionsGenerateCode(Statement s) {
+
+}
+
+void dictGenerateCode(Statement s) {
+
+}
+
+void nodeCreationGenerateCode(Statement s) {
+
+}
+
+void nodeAssignmentGenerateCode(Statement s) {
+
+}
+
+void nodeDictionaryGenerateCode(Statement s) {
+
+}
+
+void statementGenerateCode(Statement s) {
+
+}
+
+void passupExpressionCode(Expression e) {
+
+}
+
+void postfixIdentifierGenerateCode(Expression e) {
+
+}
+
+void postfixDecrementGenerateCode(Expression e) {
+
+}
+
+void postfixIncrementGenerateCode(Expresion e) {
+
+}
+
+void postfixArgumentGenerateCode(Expression e) {
+
+}
+
+void postfixBracketGenerateCode(Expression e) {
+
+}
+
+void unaryExpressionGenerateCode(Expression e) {
+
+}
+
+void castTypedExpressionGenerateCode(e) {
+
+}
+
+void multExpressionGenerateCode(Expression e) {
+
+}
+
+void addExpressionGenerateCode(Expression e) {
+
+}
+
+void relatExpressionGenerateCode(Expression e) {
+
+}
+
+void eqExpressionGenerateCode(Expression e) {
+
+}
+
+void assignmentInitExpressionGenerateCode(Expression e) {
+
+}
+
+void edgeExpressionGenerateCode(Expression e) {
+
+}
+
+void assignmentExpressionGenerateCode(Expression e) {
+
+}
+
+void primaryExpressionGenerateCode(Expression e) {
+
+}
+
+void functionExpressionGenerateCode(Expression e) {
+
+}
+
+void twoExpressionGenerateCode(Expression e) {
+
+}
+
+void identifierGenerateCode(Identifier i) {
+
+}
+
