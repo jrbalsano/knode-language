e7c5df9f3b5e40069aa03a3154dc9052730b6243
Jonathan Balsano <jrbalsano@gmail.com>
2013-05-04 15:45:47 -0400

    Added blank functions for all functions

---
+++ typecheck.c
@@ -0,0 +1,153 @@
+#include "typecheck.h"
+
+void translationUnitTypeCheck(TranslationUnit t) {
+
+}
+
+void functionDefinitionTypeCheck(FunctionDefinition f) {
+
+}
+
+void declaratorTypeCheck(Declarator d) {
+
+}
+
+void compoundStatementTypeCheck(CompoundStatement cs) {
+
+}
+
+void expressionListTypeCheck(GrammarList g) {
+
+}
+
+void statementListTypeCheck(GrammarList g) {
+
+}
+
+void parameterListTypeCheck(GrammarList g) {
+
+}
+
+void forStatementTypeCheck(Statement s) {
+
+}
+
+void whileStatementTypeCheck(Statement s) {
+
+}
+
+void ifStatementTypeCheck(Statement s) {
+
+}
+
+void ifelseStatementTypeCheck(Statement s) {
+
+}
+
+void dictlistTypeCheck(Statement s) {
+
+}
+
+void dictDefinitionsTypeCheck(Statement s) {
+
+}
+
+void dictTypeCheck(Statement s) {
+
+}
+
+void nodeCreationTypeCheck(Statement s) {
+
+}
+
+void nodeAssignmentTypeCheck(Statement s) {
+
+}
+
+void nodeDictionaryTypeCheck(Statement s) {
+
+}
+
+void statementTypeCheck(s) {
+
+}
+
+void passupExpressionType(Expression e) {
+
+}
+
+void postfixIdentifierTypeCheck(Expression e) {
+
+}
+
+void postfixDecrementTypeCheck(Expression e) {
+
+}
+
+void postfixIncrementTypeCheck(Expresion e) {
+
+}
+
+void postfixArgumentTypeCheck(Expression e) {
+
+}
+
+void postfixBracketTypeCheck(Expression e) {
+
+}
+
+void unaryExpressionTypeCheck(Expression e) {
+
+}
+
+void castTypedExpressionTypeCheck(Expression e) {
+
+}
+
+void multExpressionTypeCheck(Expression e) {
+
+}
+
+void addExpressionTypeCheck(Expression e) {
+
+}
+
+void relatExpressionTypeCheck(Expression e) {
+
+}
+
+void eqExpressionTypeCheck(Expression e) {
+
+}
+
+void assignmentInitExpressionTypeCheck(Expression e) {
+
+}
+
+void edgeExpressionTypeCheck(Expression e) {
+
+}
+
+void assignmentExpressionTypeCheck(Expression e) {
+
+}
+
+void primaryExpressionTypeCheck(Expression e) {
+
+}
+
+void functionExpressionTypeCheck(Expression e) {
+
+}
+
+void twoExpressionTypeCheck(Expression e) {
+
+}
+
+void identifierTypeCheck(Identifier i) {
+
+}
+
+void getTypeCheckType(int type) {
+
+}
