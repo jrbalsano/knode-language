91128beb5a820dacb667682fa0a03ab35e738847
Jonathan Balsano <jrbalsano@gmail.com>
2013-05-09 02:12:48 -0400

    Added commenting, removed unnecessary function

--- scope.h
+++ scope.h
@@ -13,8 +13,11 @@ struct scope_ {
 
 Scope newScope(Scope parent);
 
+/**
+ * Checks to see if a symbol exists within a scope. First checks
+ * the scope and then all the outer scopes of that scope. Return
+ * value of null indicates that the symbol does not exist.
+ */
 TypeCheckType findSymbol(Scope s, char *id);
-
-void freeScope(Scope s);
 
 #endif
