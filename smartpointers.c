1eacdb301788e4ffb8e094f6056b90651b8972b1
Jonathan Balsano <jrbalsano@gmail.com>
2013-05-11 00:59:19 -0400

    Wrote smartpointers!

---
+++ smartpointers.c
@@ -0,0 +1,114 @@
+#include "smartpointers.h"
+
+String newString(int length) {
+  String ret = (String)malloc(sizeof(struct string_));
+  ret->count = (int *)malloc(sizeof(int));
+  ret->pointer = (char *)malloc(sizeof(char *) * (length + 1));
+  ret->pointer[0] = 0;
+  *(ret->count) = 1;
+  return ret;
+}
+
+char *getString(String p) {
+  return p->pointer;
+}
+
+String copyString(String p) {
+  String ret = (String)malloc(sizeof(struct string_));
+  ret->count = p->count;
+  ret->pointer = p->pointer;
+  *(ret->count)++;
+  return ret;
+}
+
+void freeString(String p) {
+  if(--*(p->count) == 0) {
+    free(p->count);
+    free(p->pointer);
+  }
+  free(p);
+}
+
+SmartDict newDict() {
+  SmartDict ret = (SmartDict)malloc(sizeof(struct dictPointer_));
+  ret->count = (int *)malloc(sizeof(int));
+  ret->pointer = initDict();
+  *(ret->count) = 1;
+  return ret;
+}
+
+Dict getDict(SmartDict p) {
+  return p->pointer;
+}
+
+SmartDict copySmartDict(SmartDict p) {
+  SmartDict ret = (SmartDict)malloc(sizeof(struct dictPointer_));
+  ret->count = p->count;
+  ret->pointer = p->pointer;
+  *(ret->count)++;
+  return ret;
+}
+
+void freeSmartDict(SmartDict p) {
+  if(--*(p->count) == 0) {
+    free(p->count);
+    freeDict(p->pointer);
+  }
+  free(p);
+}
+
+SmartNode newNode() {
+  SmartNode ret = (SmartNode)malloc(sizeof(struct nodePointer_));
+  ret->count = (int *)malloc(sizeof(int));
+  ret->pointer = initNode();
+  *(ret->count) = 1;
+  return ret;
+}
+
+Node getNode(SmartNode p) {
+  return p->pointer;
+}
+
+SmartNode copySmartNode(SmartNode p) {
+  SmartNode ret = (SmartNode)malloc(sizeof(struct nodePointer_));
+  ret->count = p->count;
+  ret->pointer = p->pointer;
+  *(ret->count)++;
+  return ret;
+}
+
+void freeSmartNode(SmartNode p) {
+  if(--*(p->count) == 0) {
+    free(p->count);
+    freeNode(p->pointer);
+  }
+  free(p);
+}
+
+SmartEdge newEdge() {
+  SmartEdge ret = (SmartEdge)malloc(sizeof(struct edgePointer_));
+  ret->count = (int *)malloc(sizeof(int));
+  ret->pointer = initEdge();
+  *(ret->count) = 1;
+  return ret;
+}
+
+Edge getEdge(SmartEdge p) {
+  return p->pointer;
+}
+
+SmartEdge copySmartEdge(SmartEdge p) {
+  SmartEdge ret = (SmartEdge)malloc(sizeof(struct edgePointer_));
+  ret->count = p->count;
+  ret->pointer = p->pointer;
+  *(ret->count)++;
+  return ret;
+}
+
+void freeSmartEdge(SmartEdge p) {
+  if(--*(p->count) == 0) {
+    free(p->count);
+    freeEdge(p->pointer);
+  }
+  free(p);
+}
---
+++ smartpointers.h
@@ -0,0 +1,79 @@
+#ifndef __SMARTPOINTERS_H__
+#define __SMARTPOINTERS_H__
+
+#include <stdlib.h>
+
+typedef struct string_ *String;
+typedef struct dictPointer_ *SmartDict;
+typedef struct nodePointer_ *SmartNode;
+typedef struct edgePointer_ *SmartEdge;
+
+struct string_ {
+  int *count;
+  char *pointer;
+};
+
+struct dictPointer_ {
+  int *count;
+  Dict pointer;
+}
+
+struct nodePointer_ {
+  int *count;
+  Node pointer;
+}
+
+struct edgePointer_ {
+  int *count;
+  Edge pointer;
+}
+
+/**
+ * Allocates a new smartpointer for char*'s of a certain length,
+ * initializes its values, and initializes/allocates a new char *.
+ */
+String newString(int length);
+
+/**
+ * Gets the char * that a String represents
+ */
+char *getString(String p);
+
+/**
+ * Makes a new smartpointer to the same string as a previous smart
+ * pointer, s.
+ */
+String copyString(String p);
+
+/**
+ * Frees the space taken by this smartpointer, and if its the last
+ * smartpointer pointing to a particular char*, free's that char *
+ * as well.
+ */
+void freeString(String p);
+
+SmartDict newSmartDict();
+
+Dict getDict(SmartDict p);
+
+SmartDict copySmartDict(SmartDict p);
+
+void freeSmartDict(SmartDict p);
+
+SmartNode newSmartNode();
+
+Node getNode(SmartNode p);
+
+SmartNode copySmartNode(SmartNode p);
+
+void freeSmartNode(SmartNode p);
+
+SmartEdge newSmartEdge();
+
+Edge getEdge(SmartEdge p);
+
+SmartEdge copySmartEdge(SmartEdge p);
+
+void freeSmartEdge(SmartEdge p);
+
+#endif
